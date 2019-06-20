f1891ccea9b5ef1d622dadfa02f3b4f85b2d1696
Mary West <westmarye1219@gmail.com>
2019-06-20 15:05:49 -0500

    types showing up on the category card and on button click

--- src/javascripts/components/types/types.js
+++ src/javascripts/components/types/types.js
@@ -1,9 +1,32 @@
 import typesData from '../../helpers/data/typesData';
+import util from '../../helpers/util';
 
-const initTypes = (caetegoryId) => {
-  typesData.loadTypesForCategories(caetegoryId)
+const bindEvents = () => {
+  document.getElementById('toCategoriesBtn').addEventListener('click', () => {
+    document.getElementById('categories-page').classList.remove('hide');
+    document.getElementById('types-page').classList.add('hide');
+  });
+};
+
+const writeTypes = (types) => {
+  let domString = '';
+  types.forEach((type) => {
+    domString += '<div class="col-3">';
+    domString += `<div id='${type.name}' class="card p-2">`;
+    domString += '<div class="card-body">';
+    domString += `<h5 class="card-title">${type.description}</h5>`;
+    domString += '</div>';
+    domString += '</div>';
+    domString += '</div>';
+  });
+  util.printToDom('types-on-categories', domString);
+};
+
+const initTypes = (categoryId) => {
+  bindEvents();
+  typesData.loadTypesForCategories(categoryId)
     .then((types) => {
-      console.error('all types', types);
+      writeTypes(types);
     })
     .catch(err => console.error(err));
 };
--- src/javascripts/helpers/data/typesData.js
+++ src/javascripts/helpers/data/typesData.js
@@ -10,4 +10,20 @@ const loadTypesForCategories = categoryId => new Promise((resolve, reject) => {
     .catch(err => reject(err));
 });
 
-export default { loadTypesForCategories };
+const getTypesForEachCategory = categories => new Promise((resolve, reject) => {
+  axios.get('../db/types.json')
+    .then((resp) => {
+      const { types } = resp.data;
+      const categoriesWithTypes = categories.map((category) => {
+        const newCategory = category;
+        const matchingTypes = types.filter(type => type.categoryId === category.id);
+        newCategory.types = matchingTypes;
+        return newCategory;
+      });
+
+      resolve(categoriesWithTypes);
+    })
+    .catch(err => reject(err));
+});
+
+export default { loadTypesForCategories, getTypesForEachCategory };

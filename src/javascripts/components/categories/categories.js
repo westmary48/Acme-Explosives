1f681f935d9cbecf6d4598a7e7be1a190e199d14
Mary West <westmarye1219@gmail.com>
2019-06-20 14:30:13 -0500

    type button is on category card and goes back

--- src/javascripts/components/categories/categories.js
+++ src/javascripts/components/categories/categories.js
@@ -1,6 +1,20 @@
 import categoriesData from '../../helpers/data/categoriesData';
 import util from '../../helpers/util';
 
+const seeTypeDiv = (e) => {
+  const boardId = e.target.closest('.card').id;
+  console.error('you clicked a button!', boardId);
+  document.getElementById('categories-page').classList.add('hide');
+  document.getElementById('types-page').classList.remove('hide');
+};
+
+const bindEvents = () => {
+  const allButtons = document.getElementsByClassName('see-types');
+  for (let i = 0; i < allButtons.length; i += 1) {
+    allButtons[i].addEventListener('click', seeTypeDiv);
+  }
+};
+
 const writeCategories = (categories) => {
   let domString = '';
   categories.forEach((category) => {
@@ -8,11 +22,13 @@ const writeCategories = (categories) => {
     domString += `<div id='${category.id}' class="card p-2">`;
     domString += '<div class="card-body">';
     domString += `<h5 class="card-title">${category.name}</h5>`;
+    domString += '<button class="btn btn-warning see-types">Types</button>';
     domString += '</div>';
     domString += '</div>';
     domString += '</div>';
   });
   util.printToDom('user-categories', domString);
+  bindEvents();
 };
 
 const initCategories = () => {
@@ -23,4 +39,4 @@ const initCategories = () => {
     .catch(err => console.error('error from loadCategories', err));
 };
 
-export default { writeCategories,initCategories };
+export default { initCategories };
